8fb0db3bf9644a9ca850a47ef0658767760ca685
mheppler-rh <64473624+mheppler-rh@users.noreply.github.com>
2023-11-30 00:44:23 +0100

    Sync with actual ACM versions, better code
    * More commits from original repo:
    * just a small typos
    * More optimalizations, added checking ACM version for updates, prepared for gather data from managed cluster - need enforce ACM version via -m option.
    * Added script versioning - to be able check which version customer used...
    * Updated README.md - added disclaimer, requirements and other minor changes
    Tagged v1.0
    * Updated latest releases to actual
    * Added support for define own registry via param -r.
    Usable in disconnected env. Updated doc, version and generation.
    * Added support for collecting submariner data
    - To collect submariner data use -s option.
    Various fixes and optimalizations - dryrun, ACM_CHANNEL...
    * Added parameter -l for requesting longer timeout. More in application help
    * RHACM v2.8 is alive, so updated upgrade path
    * Updated README.md - support for 2.8 version, and updated info about -r param.
    * acm_create-must-gather.sh - small typo's
    * Added detection of successful download must-gather image from registry and option -M for enforce MCE must-gather.
    * More changes: MCE gathering optimalization, new versions
    It is not neccesary to collect separate ACM and MCE images for latest
    m-g images of supported versions - MCE is a part of ACM now.
    Updated latest versions of ACM, 2.4 and 2.5 are EOL now, 2.6 will be EOL soon.
    * Revert "Merge branch 'main' into main"
    2555.
    * Added support for latest verions of ACM - 2.9.1, 2.8.2, 2.7.9, 2.6.7
    * Updated README
    ---------
    Co-authored-by: Waldirio M Pinheiro <waldirio@gmail.com>

--- acm_create-must-gather.sh
+++ acm_create-must-gather.sh
@@ -10,18 +10,15 @@ _red_="${Esc}[0;31m" #set red
 _boldred_="${Esc}[0;1;31m" #set bold and red.
 
 REL24=99
-REL25=9
-REL26=6
-REL27=4
-REL28=0
+REL25=99
+REL26=7
+REL27=9
+REL28=2
+REL29=1
 
-MCEFIX25=7
-MCEFIX26=5
-MCEFIX27=3
-
-GENERATION=1687176791
+GENERATION=1701180163
 RETIRED=7689599
-VERSION="1.2"
+VERSION="1.5"
 SOURCE="https://github.com/C-RH-C/ACM-MCE-must-gather/blob/main/acm_create-must-gather.sh"
 
 isretired() {
@@ -72,15 +69,7 @@ isupdate() {
 					UPDOC="Upgrade to 2.5 version: https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.5/html/install/index"
 				;;
 				5)
-					echo "THIS VERSION WILL BE EOL SOON, PLEASE UPGRADE TO 2.6"
-					if [ "${ver[2]}" -lt "$REL25" ]; then
-						echo "Upgrade available to 2.5.$REL25 or 2.6"
-					else
-						echo "Upgrade available to 2.6"
-					fi
-					if [ "${ver[2]}" -lt "7" ]; then
-						AFFECTED_CVE_2023_29017="yes"
-					fi
+					echo "THIS VERSION IS NO LONGER SUPPORTED, PLEASE UPGRADE TO 2.6"
 					UPDOC="Upgrade to 2.6 version: https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.6/html/install/index"
 				;;
 				6)
@@ -111,6 +100,16 @@ isupdate() {
 				8)
 					if [ "${ver[2]}" -lt "$REL28" ]; then
 						echo "Upgrade available to 2.8.$REL28"
+						UPDOC=""
+					else
+						echo "Upgrade available to 2.9"	
+					fi
+					UPDOC="Upgrade to 2.9 version: https://access.redhat.com/documentation/en-us/red_hat_advanced_cluster_management_for_kubernetes/2.9/html/install/index"
+
+				;;
+				9)
+					if [ "${ver[2]}" -lt "$REL29" ]; then
+						echo "Upgrade available to 2.9.$REL29"
 						UPDOC=""
 					fi
 				;;
@@ -150,26 +149,22 @@ mcemgimage() {
 			case ${ver[1]} in
 				4) VERSION='-'
 				;;
-				5) if [ "${ver[2]}" -lt "$MCEFIX25" ]; then
-					echo "${REGISTRY}/multicluster-engine/must-gather-rhel8:v2.0"
-				   else
-					echo '-'
+				5) VERSION="2.0"
+				;;
+				6) if [ "x${ENFORCE}" == "xyes" ]; then
+					VERSION="2.1"
 				   fi
 				;;
-				6) if [ "${ver[2]}" -lt "$MCEFIX26" ]; then
-					echo "${REGISTRY}/multicluster-engine/must-gather-rhel8:v2.1"
-				   else
-					echo '-'
-				   fi
-				;;
-				7) if [ "${ver[2]}" -lt "$MCEFIX27" ]; then
-					echo "${REGISTRY}/multicluster-engine/must-gather-rhel8:v2.2"
-				   else
-					echo '-'
+				7) if [ "x${ENFORCE}" == "xyes" ]; then
+					VERSION="2.2"
 				   fi
 				;;
 				8) if [ "x${ENFORCE}" == "xyes" ]; then
 					VERSION="2.3"
+				   fi
+				;;
+				9) if [ "x${ENFORCE}" == "xyes" ]; then
+					VERSION="2.4"
 				   fi
 				;;
 				*) VERSION='-'
@@ -196,8 +191,15 @@ REGISTRY='registry.redhat.io'
 OWNREGISTRY='-'
 SUBCTL='-'
 LONGRUN=''
+MCE_ENFORCE='-'
 
-while getopts tsd:m:r:l:h flag
+###
+#
+# Parameter -M not documented, enforce creating MCE m-g
+#
+###
+
+while getopts tsMd:m:r:l:h flag
 do
     case "${flag}" in
         t) DRY_RUN=yes;;
@@ -212,6 +214,7 @@ do
 	   ;;
 	s) SUBCTL=yes;;
 	l) LONGRUN="--request-timeout=${OPTARG}";;
+	M) MCE_ENFORCE='yes';;
     esac
 done
 
@@ -330,10 +333,20 @@ echo "${_bold_}Creating must-gather for ACM to '${DIR}/acm/'${_norm_}"
 if [ "XXX$DRY_RUN" == "XXXyes" ];
 then
 	echo "mkdir -p \"${DIR}/acm/\""
-	echo "oc adm must-gather ${LONGRUN} --image=\"${ACM_IMAGE}\" --dest-dir=\"${DIR}/acm/\" &> \"${DIR}/acm-must-gather.log\""
+	echo "oc adm must-gather ${LONGRUN} --image=\"${ACM_IMAGE}\" --dest-dir=\"${DIR}/acm/\" |& tee \"${DIR}/acm-must-gather.log\" | grep -q -m 1 'OUT gather did not start: unable to pull image: ImagePullBackOff: Back-off pulling image'"
 else
 	mkdir -p "${DIR}/acm/"
-	oc adm must-gather ${LONGRUN} --image="${ACM_IMAGE}" --dest-dir="${DIR}/acm/" &> "${DIR}/acm-must-gather.log"
+	oc adm must-gather ${LONGRUN} --image="${ACM_IMAGE}" --dest-dir="${DIR}/acm/" |& tee "${DIR}/acm-must-gather.log" | grep -q -m 1 'OUT gather did not start: unable to pull image: ImagePullBackOff: Back-off pulling image'
+
+	if [ $? -eq 0 ];
+	then
+		echo "---------------------------------------------"
+		echo "Unable to download image \"acm-must-gather-rhel8:v${ACM_CHANNEL#release-}\" from registry \"${REGISTRY}\""
+		echo "Please, use ${_bold_}-r${_norm_} parameter to specify your registry and run script again"
+		echo "For details, please use '$0 -h'"
+		echo "---------------------------------------------"
+		exit 10
+	fi
 fi
 
 echo "${_bold_}ACM must-gather done${_norm_}"
@@ -348,7 +361,17 @@ then
 		echo "oc adm must-gather ${LONGRUN} --image=\"${MCE_IMAGE}\" --dest-dir=\"${DIR}/mce/\" &> \"${DIR}/mce-must-gather.log\""
 	else
 		mkdir -p "${DIR}/mce/"
-		oc adm must-gather ${LONGRUN} --image="${MCE_IMAGE}" --dest-dir="${DIR}/mce/" &> "${DIR}/mce-must-gather.log"
+		oc adm must-gather ${LONGRUN} --image="${MCE_IMAGE}" --dest-dir="${DIR}/mce/" |& tee "${DIR}/mce-must-gather.log" | grep -q -m 1 'OUT gather did not start: unable to pull image: ImagePullBackOff: Back-off pulling image'
+
+		if [ $? -eq 0 ];
+		then
+			echo "---------------------------------------------"
+			echo "Unable to download image \"multicluster-engine/must-gather-rhel8:v2.X\" from registry \"${REGISTRY}\""
+			echo "Please, use ${_bold_}-r${_norm_} parameter to specify your registry and run script again"
+			echo "For details, please use '$0 -h'"
+			echo "---------------------------------------------"
+			exit 10
+		fi
 	fi
 
 	echo "${_bold_}MCE must-gather done${_norm_}"
